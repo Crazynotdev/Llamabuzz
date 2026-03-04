564833843d9bba6a20f003b199a3a56ab30ac18f
ZynXx <techigwe@gmail.com>
2026-03-04 21:39:30 +0100

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,13 +1,5 @@
-'use client'
-
 import './globals.css'
-// ⚠️ obligatoire pour tous les composants client
-
-import Layout from "@/components/Layout"
-import Link from "next/link"
-import { supabase } from "@/lib/supabase"
-import { useEffect, useState, ReactNode } from "react"
-//import { ReactNode } from 'react'
+import { ReactNode } from 'react'
 
 export const metadata = {
   title: 'LLAMABUZZ',
